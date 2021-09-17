9f98bffa2b06b855ce9a3144d51dd5b2ef182f5d
Theodor Brown <brown5u@etu.univ-lorraine.fr>
2021-09-17 12:30:53 +0200

    carte

---
+++ carte/file.js
@@ -0,0 +1,67 @@
+/*let list = [
+    ['Auvergne-Rhône-Alpes', 'FR-ARA', 21]
+    ['Bourgogne-Franche-Comté','FR-BFC', 31]
+    ['Bretagne','FR-BRE	', 33]
+    ['Corse','FR-COR', 12]
+    ['Centre-Val de Loire','FR-CVL',18]
+    ['Grand Est', 'FR-GES', 22]
+    ['Hauts-de-France', 'FR-HDF', 38]
+    ['Île-de-France', 'FR-IDF', 34]
+    ['Normandie', 'FR-NOR', 4]
+    ['Nouvelle-Aquitaine', 'FR-NAQ', 17]
+    ['Occitanie', 'FR-OCC', 28]
+    ['Pays de la Loire', 'FR-PDL', 30]
+    ['Provence-Alpes-Côte dAzur', 'FR-PAC', 9]
+];
+*/
+
+function color(code) {
+
+    var el = document.getElementById(code);
+    el.style.fill = "red";
+/*
+    switch (code) {
+        case 'FR-ARA':
+            //todo
+            break;
+        case 'FR-BFC':
+            //todo
+            break;
+        case 'FR-BRE':
+            //todo
+            break; 
+        case 'FR-COR':
+            //todo
+            break;
+        case 'FR-CVL':
+            //todo
+            break;
+        case 'FR-GES':
+            //todo
+            break;        
+        case 'FR-HDF':
+            //todo
+            break;
+        case 'FR-IDF':
+            //todo
+            break;
+        case 'FR-NOR':
+            //todo
+            break;
+        case 'FR-NAQ':
+            //todo
+            break;        
+        case 'FR-OCC':
+            //todo
+            break;
+        case 'FR-PDL':
+            //todo
+            break;
+        case 'FR-PAC':
+            //todo
+            break;
+ }
+ */
+}
+
+color('FR-ARA');
