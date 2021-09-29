ef83230f409dbad4edeb0c3f534ee2859304495d
Theodor Brown <brown5u@etu.univ-lorraine.fr>
2021-09-29 12:06:26 +0200

    exos js suite

--- exo_tableau/file.js
+++ exo_tableau/file.js
@@ -1,54 +1,19 @@
-let list = [
-    {nom:'Auvergne-Rhône-Alpes', code: 'FR-ARA', taux: 21},
-    {nom:'Bourgogne-Franche-Comté', code: 'FR-BFC', taux: 31},
-    {nom:'Bretagne', code: 'FR-BRE', taux: 33},
-    {nom:'Corse', code: 'FR-COR', taux: 12},
-    {nom:'Centre-Val de Loire', code: 'FR-CVL', taux: 18},
-    {nom:'Grand Est', code: 'FR-GES', taux: 22},
-    {nom:'Hauts-de-France', code: 'FR-HDF', taux: 38},
-    {nom:'Île-de-France', code: 'FR-IDF', taux: 34},
-    {nom:'Normandie', code: 'FR-NOR', taux: 4},
-    {nom:'Nouvelle-Aquitaine', code: 'FR-NAQ', taux: 17},
-    {nom:'Occitanie', code: 'FR-OCC', taux: 28},
-    {nom:'Pays de la Loire', code: 'FR-PDL', taux: 30},
-    {nom:'Provence-Alpes-Côte dAzur', code: 'FR-PAC', taux: 9}
-];
-
-
-const couleurs = [
-    {couleur:"#ffffe5", mini:0, maxi:5 },
-    {couleur:"#f7fcb9", mini:5, maxi:10 },
-    {couleur:"#d9f0a3", mini:10, maxi:15 },
-    {couleur:"#addd8e", mini:15, maxi:19 },
-    {couleur:"#78c679", mini:19, maxi:23 },
-    {couleur:"#41ab5d", mini:23, maxi:27 },
-    {couleur:"#238443", mini:27, maxi:31 },
-    {couleur:"#006837", mini:31, maxi:35 },
-    {couleur:"#004529", mini:35, maxi:40 }
-]
-
-function color() {
-    for(region of list) {
-        const ligne = couleurs.find(c => c.maxi > region.taux);
-        document.getElementById(region.code).style.fill = ligne.couleur;
-    }
-}
 let tableau = [
-    {code: 11, nom:	"Île-de-France", BTS:44049, DUT:17093, MES:28830, Ingenieur:42347},
-    {code: 24, nom:	"Centre-Val de Loire", BTS:8324, DUT:4511, MES:5171, Ingenieur:3071},
-    {code: 27, nom:	"Bourgogne-Franche-Comté", BTS:10102, DUT:5012, MES:5180, Ingenieur:4899},
-    {code: 28, nom:	"Normandie", BTS:11885, DUT:7011, MES:6471, Ingenieur:6497},
-    {code: 32, nom:	"Hauts-de-France", BTS:26320, DUT:10611, MES:14485, Ingenieur:16115},
-    {code: 44, nom:	"Grand Est", BTS:21352, DUT:12778, MES:12215, Ingenieur:14102},
-    {code: 52, nom:	"Pays de la Loire", BTS:16704, DUT:5716, MES:5780, Ingenieur:10292},
-    {code: 53, nom:	"Bretagne", BTS:15467, DUT: 6653, MES:5835, Ingenieur:8853},
-    {code: 75, nom:	"Nouvelle-Aquitaine", BTS:21606, MES:10082, DUT:11289, Ingenieur:8718},
-    {code: 76, nom:	"Occitanie", BTS:24394, DUT:10578, MES:9980, Ingenieur:14641},
-    {code: 84, nom:	"Auvergne-Rhône-Alpes", BTS:28795, DUT:17320, MES:14938, Ingenieur:23272},
-    {code: 93, nom:	"Provence-Alpes-Côte d'Azur", BTS:17893,DUT:7780, MES:10871, Ingenieur:5774}
+    {code: 'FR-IDF', nom:	"Île-de-France", BTS:44049, DUT:17093, MES:28830, Ingenieur:42347},
+    {code: 'FR-CVL', nom:	"Centre-Val de Loire", BTS:8324, DUT:4511, MES:5171, Ingenieur:3071},
+    {code: 'FR-BFC', nom:"Bourgogne-Franche-Comté", BTS:10102, DUT:5012, MES:5180, Ingenieur:4899},
+    {code: 'FR-NOR', nom:	"Normandie", BTS:11885, DUT:7011, MES:6471, Ingenieur:6497},
+    {code: 'FR-HDF', nom:	"Hauts-de-France", BTS:26320, DUT:10611, MES:14485, Ingenieur:16115},
+    {code: 'FR-GES', nom:	"Grand Est", BTS:21352, DUT:12778, MES:12215, Ingenieur:14102},
+    {code: 'FR-PDL', nom:	"Pays de la Loire", BTS:16704, DUT:5716, MES:5780, Ingenieur:10292},
+    {code: 'FR-BRE', nom:	"Bretagne", BTS:15467, DUT: 6653, MES:5835, Ingenieur:8853},
+    {code: 'FR-NAQ', nom:	"Nouvelle-Aquitaine", BTS:21606, MES:10082, DUT:11289, Ingenieur:8718},
+    {code: 'FR-OCC', nom:	"Occitanie", BTS:24394, DUT:10578, MES:9980, Ingenieur:14641},
+    {code: 'FR-BFC', nom:	"Auvergne-Rhône-Alpes", BTS:28795, DUT:17320, MES:14938, Ingenieur:23272},
+    {code: 'FR-PAC', nom:	"Provence-Alpes-Côte d'Azur", BTS:17893,DUT:7780, MES:10871, Ingenieur:5774}
 ]
 
-const couleurs2 = [
+const palette = [
     {codec:"#8e0152", min:3000, max:5000},
     {codec:"#c51b7d", min:5000, max:6000},
     {codec:"#de77ae", min:6000, max:10000},
@@ -61,9 +26,9 @@ const couleurs2 = [
     {codec:"#276419", min:30000, max:45000}
 ]
 //remplir le tableau avec ses valeurs
-function remplir() {
+function remplir(tab) {
     let contenu = "";
-    for(region of tableau) {
+    for(region of tab) {
         contenu += `<tr>
             <td>${region.code}</td><td>${region.nom}</td>
             <td>${region.BTS}</td>
@@ -75,24 +40,32 @@ function remplir() {
     }
     document.getElementById("databody").innerHTML = contenu;
 }
-remplir();
+remplir(tableau);
 
 //Tier et afficher le tableau par rapport à l'effectif en DUT
 function tri() {
     tableau.sort((el1,el2)=> el1.DUT - el2.DUT);
-    remplir();
+    remplir(tableau);
 }
 
 //Afficher les régions qui ont moins d'étudiants en école d'ingénieur qu'en DUT
-function ingenieur_dut () {
+function ingenieur_dut() {
     for(lign of tableau) {
         const ligne = tableau.find(t => t.DUT > t.Ingenieur);
-        for(lign2 of list) {
-            lign2.nom
-        }
-        ligne.nom
-        list.nom
-        list.code
-        document.getElementById(region.code).style.fill = ligne.couleur;
+        document.getElementById(ligne.code).style.fill = "red";
     }
+}
+
+//liste des fonctions
+const fonctions = [
+    {"code":"tri", "fonction": tri()},
+    {"code":"inge_dut", "fonction": ingenieur_dut()},
+]
+
+//select html
+function appliquer() {
+    let valueselect = document.getElementById("question").value
+    let ligne = fonctions.find(f => f.code == valueselect);
+    let t = ligne.fonction();
+    remplir(t);
 }
