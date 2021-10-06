0b7e96b786604b03ef347828e003fce9f43a893e
theodorbrown <theodor.brown01@gmail.com>
2021-10-06 12:10:35 +0200

    refait carte perdue

--- exo_async/file.js
+++ exo_async/file.js
@@ -1,3 +1,21 @@
-function charger() {
-  
+var map = L.map('macarte').setView([48.03795, 6.97124], 11.3);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+async function getStations() {
+  let data = await fetch("https://workshop.neotechweb.net/ws/skimap/1.0.0/stations.php?massif=2");
+  let datajson = await data.json();
+  console.log(datajson)
+
+  let resultats =  datajson.filter(el => !!el.lng && !!el.lat)
+  resultats.forEach(element => {
+        L.marker([element.lat, element.lng])
+                    .addTo(map)
+                    .bindPopup(element.nom)
+  })
 }
+
+getStations()
--- exo_drapeaux/file.js
+++ exo_drapeaux/file.js
@@ -85,3 +85,6 @@ function charger() {
    */
 
   
+async function displayStations() {
+  
+}
