b8083afcc2afb5085ee3e7c3420a8a1cf523be89
Theodor Brown <theodorbrown@MacBook-Air-de-Theodor.home>
2021-11-12 14:28:43 +0100

    corrections et améliorations

--- Ski/file.js
+++ Ski/file.js
@@ -1,36 +1,44 @@
 document.addEventListener('init', function(event) {
+    //ons-page (event.target)
     var page = event.target;
+    const navigator = document.getElementById("myNavigator");
   
     if (page.id === 'page1') {
          ChargerMassifs(page);
     }
     else if (page.id === 'page2') {
-        ChargerStations(page)
+        ChargerStations(page);
+        page.querySelector('ons-toolbar .center').innerHTML += " " + page.data.title;
     }
     else if (page.id === 'page3') {
-        page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+        page.querySelector('ons-toolbar .center').innerHTML += " " + page.data.title;
+    }
+    else if (page.id === 'tab1') {
+        console.log(navigator.topPage.data.title);
     }
 });
 
-
+//2
 function goPageStations(event) {
   document.querySelector('#myNavigator').pushPage('page2.html',
   { data : { 
-        title : event.currentTarget.dataset.name,
+        title : event.currentTarget.dataset.nom,
         id: event.currentTarget.dataset.id
     }
   });
 }
 
+//4
 function goPageStation(event) {
     document.querySelector('#myNavigator').pushPage('page3.html',
-    { data : { 
+    { data : {
           title : event.currentTarget.dataset.nom,
           id: event.currentTarget.dataset.id
       }
     });
 }
 
+//1
 async function ChargerMassifs(page) {
     const data = await fetch("https://workshop.neotechweb.net/ws/skimap/1.0.0/massifs.php");
     const json = await data.json();
@@ -40,9 +48,9 @@ async function ChargerMassifs(page) {
     json.forEach(elt => {
         const clone = document.importNode(template.content, true);
 
-        const onlistitem = clone.children[0]; // Premier enfant
-        onlistitem.dataset.id = elt.id;
-        onlistitem.dataset.nom = elt.nom;
+        const onslistitem = clone.children[0]; // Premier enfant -> item complet
+        onslistitem.dataset.id = elt.id;
+        onslistitem.dataset.nom = elt.nom;
 
         const items = clone.querySelectorAll("span");
         items[0].textContent = elt.nom;
@@ -52,6 +60,7 @@ async function ChargerMassifs(page) {
     });
 }
 
+//3
 async function ChargerStations(page) {
     const data = await fetch(`https://workshop.neotechweb.net/ws/skimap/1.0.0/stations.php?massif=${page.data.id}`);
     const json = await data.json();
